9d43e596922f19adc0b6c95d8a9c0fb7cf28ad8e
wkropat <wkropat@gmail.com>
2021-10-04 10:36:49 -0700

    added js pseudocode

---
+++ assets/script.js
@@ -0,0 +1,33 @@
+// Code for the recipe builder site
+
+// Initialize
+function init {
+    // Load any ingredients that were previously entered and autofill the ingredients list
+
+}
+
+// Get Recipe Button
+
+function getRecipe {
+    // Calls spoonacular api with the inputs as ingredients 
+    // gets 5 recipes back in order of most ingredients used
+    // if recipe uses more of an ingredient than the use has on hand, 
+        // 
+
+}
+
+// Surprise Me button
+
+function surpriseMe {
+    // Gets one random recipe from list above
+    
+
+}
+// Make cards
+
+function renderCards {
+    // Build the cards with data sourced from the spoonacular API
+
+}
+
+init()
