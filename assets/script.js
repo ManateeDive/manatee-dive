5b456755ebb84444de12f6bb1df3eefcf920a344
Sami Byers <samtaylor.byers@gmail.com>
2021-10-04 19:04:55 -0500

    need help with api

--- assets/script.js
+++ assets/script.js
@@ -20,29 +20,30 @@ getRecipe.addEventListener('click', getApi);
 
 // Code for the recipe builder site
 
-// Initialize
-function init {
-    // Load any ingredients that were previously entered and autofill the ingredients list
+// // Initialize
+// function init (){
+//     // Load any ingredients that were previously entered and autofill the ingredients list
 
-}
+// }
 
-// Get Recipe Button
+// // Get Recipe Button
 
-function getRecipe {
-    // Calls spoonacular api with the inputs as ingredients 
-    // gets 5 recipes back in order of most ingredients used
-    // if recipe uses more of an ingredient than the use has on hand, 
-        // 
+// function getRecipe (){
+//     // Calls spoonacular api with the inputs as ingredients 
+//     // gets 5 recipes back in order of most ingredients used
+//     // if recipe uses more of an ingredient than the use has on hand, 
+//         // 
 
-}
+// }
 
-// Surprise Me button
+// Surprise Me variables
 var surpriseRecipe = document.getElementById('surpriseRecipe');
+var surpriseResult = document.getElementById('surpriseResult');
 
-function surpriseMe {
+function surpriseMe (){
     // Gets one random recipe from list above
     var requestUrl = "https://api.spoonacular.com/recipes/random?apiKey=6b2994b0da2e49f2a7e66de1133a594f"
-
+    
     fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -51,7 +52,8 @@ function surpriseMe {
       for (var i = 0; i < data.length; i++) {
         var listItem = document.createElement('li');
         listItem.textContent = data[i].html_url;
-        recipeList.appendChild(listItem);
+        surpriseResult.appendChild(listItem);
+        
       }
     });
 }
@@ -59,16 +61,16 @@ surpriseRecipe.addEventListener('click', surpriseMe);
 
 // Make cards
 
-function renderCards {
-    // Build the cards with data sourced from the spoonacular API
+// function renderCards (){
+//     // Build the cards with data sourced from the spoonacular API
 
-}
+// }
 
-init()
-// Add autocomplete for ingredients list
-// STILL NEED TO ADD DATA LIST with keys [ap = apples] etc.
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, options);
-  });
+// init()
+// // Add autocomplete for ingredients list
+// // STILL NEED TO ADD DATA LIST with keys [ap = apples] etc.
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.autocomplete');
+//     var instances = M.Autocomplete.init(elems, options);
+//   });
 
